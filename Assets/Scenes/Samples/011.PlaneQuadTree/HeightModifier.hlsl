0227eab89f769497571be2e3b585c844103e32f6
Vincent Thémereau <v@themereau.com>
2022-03-09 22:18:51 +0100

    wip seams on vertex shader 2/N

--- Assets/Scenes/Samples/011.PlaneQuadTree/HeightModifier.hlsl
+++ Assets/Scenes/Samples/011.PlaneQuadTree/HeightModifier.hlsl
@@ -8,12 +8,12 @@
 // left, up, right, down
 float4 depthDeltaData[ARRAY_SIZE];
 
-int indexMask[44] = {
+const float indexMask[44] = {
 	//  0  1  2  3  4  5  6  7  8  9  10
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0 // depthDelta = 0
-		0, 1, 0, 1, 0, 1, 0, 1, 0, 1,  0 // depthDelta = 1
-		0, 1, 1, 1, 1, 0, 1, 1, 1, 1,  0 // depthDelta = 2
-		0, 1, 1, 1, 1, 1, 1, 1, 1, 1,  0 // depthDelta = 3
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, // depthDelta = 0
+		0, 1, 0, 1, 0, 1, 0, 1, 0, 1,  0, // depthDelta = 1
+		0, 1, 1, 1, 1, 0, 1, 1, 1, 1,  0, // depthDelta = 2
+		0, 1, 1, 1, 1, 1, 1, 1, 1, 1,  0, // depthDelta = 3
 	};
 
 void HeightModifier_float(float3 vOS, float3 vWS, float heightVScale, float heightHScale, out float3 vOutOS)
@@ -22,9 +22,10 @@ void HeightModifier_float(float3 vOS, float3 vWS, float heightVScale, float heig
 #if UNITY_ANY_INSTANCING_ENABLED
 	height = ClassicNoise(vWS * heightHScale);
 	float4 depthDelta = depthDeltaData[unity_InstanceID];
-	if (vOS.x + 4 <= 0.001)
+	//if ( <= 0.001)
 	{
-		height = 0;
+		//height = indexMask[floor(depthDelta.x) * 10 + floor(vOS.x + 5)];
+		height = depthDelta.x;// * 10 + floor(vOS.x + 5)];
 	}
 	//height += unity_InstanceID;
 #endif
