772eabc6a67a08ad5fac24fa51ef646000cff6aa
Vincent Thémereau <v@themereau.com>
2022-04-19 22:32:56 +0200

    worm looks nice. need to use a compute buffer to create the worm data

--- Assets/Scenes/Samples/013.TesselatedWorm/WormMesh.cs
+++ Assets/Scenes/Samples/013.TesselatedWorm/WormMesh.cs
@@ -3,6 +3,8 @@ using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+using static UnityEngine.Rendering.HableCurve;
+
 namespace _013_TesselatedWorm
 {
 	[System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
@@ -16,10 +18,9 @@ namespace _013_TesselatedWorm
 	[ExecuteInEditMode]
 	public class WormMesh : MonoBehaviour
 	{
+		public PerlinWorm perlinWorm;
+
 		[Range(1, 128)] public int radialSegmentCount = 6;
-		[Range(1, 1024f)] public float segmentLength = 10f;
-		[Range(1, 16384f)] public float length = 100f;
-		[Range(1, 512)] public float radius = 5f;
 
 		public float gizmoPointSize = 0.1f;
 
@@ -27,6 +28,10 @@ namespace _013_TesselatedWorm
 		public float height = 1f;
 
 		private Mesh mesh = null;
+
+		private float Length => perlinWorm.config.length;
+		private float SegmentLength => perlinWorm.config.stepLength * 100;
+		private float Radius => perlinWorm.config.wormRadius;
 
 		// Start is called before the first frame update
 		private void Start()
@@ -36,10 +41,27 @@ namespace _013_TesselatedWorm
 		// Update is called once per frame
 		private void Update()
 		{
+			perlinWorm.Update();
 			if (mesh == null)
 			{
 				AllocateMeshInfo();
 			}
+
+			Vector4[] vPos = new Vector4[perlinWorm.Count];
+			Vector4[] vForward = new Vector4[perlinWorm.Count];
+			for (int i = 0; i < vPos.Length; ++i)
+			{
+				vPos[i] = perlinWorm.GetPosition(i);
+				vForward[i] = perlinWorm.GetForward(i);
+			}
+
+			Material mat = GetComponent<MeshRenderer>().sharedMaterial;
+			mat.SetVectorArray("_PositionArray", vPos);
+			mat.SetVectorArray("_ForwardArray", vForward);
+			mat.SetInt("_ArrayCount", perlinWorm.Count);
+			mat.SetFloat("_Radius", Radius);
+			mat.SetFloat("_Length", Length);
+
 			GenerateMesh();
 		}
 
@@ -91,7 +113,7 @@ namespace _013_TesselatedWorm
 		public int VertexCount => XCount * ZCount;
 		public int IndiceCount => (XCount - 1) * (ZCount - 1) * 2 * 3;
 		public int XCount => radialSegmentCount + 1;
-		public int ZCount => Mathf.CeilToInt(length / segmentLength) + 1;
+		public int ZCount => Mathf.CeilToInt(Length / SegmentLength) + 1;
 
 		private void GenerateMesh()
 		{
@@ -110,8 +132,16 @@ namespace _013_TesselatedWorm
 				{
 					var i = x + z * xCount;
 					float angle = Mathf.PI * 2 * x / (radialSegmentCount);
-					Vector3 pOnCircle = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
-					Vector3 p = pOnCircle * radius + new Vector3(0, 0, z * segmentLength);
+					//Vector3 pOnPath = perlinWorm.GetPositionNormalized(z / (float)(zCount - 1));
+					//Vector3 pOnCircle = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+					//Vector3 p = pOnCircle * Radius + pOnPath; // new Vector3(0, 0, z * SegmentLength);
+
+					float t = z / (float)(zCount - 1);
+					Vector3 pOnPath = perlinWorm.GetPositionNormalized(t);
+					//Vector3 tan = perlinWorm.GetForwardNormalized(t);
+					//Quaternion q = Quaternion.FromToRotation(Vector3.forward, tan);
+					Vector3 pOnCircle = new Vector3(Mathf.Cos(angle) * Radius, Mathf.Sin(angle) * Radius, 0);
+					Vector3 p = pOnPath + pOnCircle;
 
 					vertices[i] = new Vertex
 					{
