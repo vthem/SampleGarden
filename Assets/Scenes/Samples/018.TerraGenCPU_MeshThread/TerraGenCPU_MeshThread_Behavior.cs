7d5f5fc58b51b19327240460a6a68f33b6e80425
Vincent Thémereau <v@themereau.com>
2022-06-26 21:04:01 +0200

    struggling to use jobs

--- Assets/Scenes/Samples/018.TerraGenCPU_MeshThread/TerraGenCPU_MeshThread_Behavior.cs
+++ Assets/Scenes/Samples/018.TerraGenCPU_MeshThread/TerraGenCPU_MeshThread_Behavior.cs
@@ -4,6 +4,9 @@ using Unity.Collections;
 using UnityEngine.Rendering;
 using System;
 using Unity.Jobs;
+using UnityEngine.UIElements;
+using static UnityEngine.InputManagerEntry;
+using Unity.Jobs.LowLevel.Unsafe;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -26,9 +29,6 @@ namespace _018_TerraGenCPU_MeshThread
 			public Vector2 uv;
 		}
 
-		private NativeArray<VertexData> vertices;
-		private NativeArray<uint> indices;
-
 		private VertexAttributeDescriptor[] vertexLayout = new[]
 		{
 			new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3),
@@ -36,22 +36,49 @@ namespace _018_TerraGenCPU_MeshThread
 		};
 
 
-		//private struct CreateMeshJob : IJobParallelFor
-		//{
-		//	public NativeArray<VertexData> vertices;
-		//	public NativeArray<uint> indices;
-		//	[ReadOnly] public NativeArray<Color32> heightMapData;
-		//	public Vector2Int pixelCount;
-		//	public Vector2Int vertexCount;
+		private struct MeshJob : IJob
+		{
+			public NativeArray<VertexData> vertices;
+			public NativeArray<uint> indices;
+			[ReadOnly] public NativeArray<RGB> heightData;
+			[ReadOnly] public Vector2Int count;
+			[ReadOnly] public Vector2 size;
+			[ReadOnly] public Vector2Int textureSize;
+			[ReadOnly] public float heightScale;
+			[ReadOnly] public int offset;
 
-		//	public void Execute(int index)
-		//	{
-		//		Vector2Int pixelXY = Utils.GetXYFromIndex(index, vertexCount.x);
+			public void Execute()
+			{
+				for (int vertexIndex = 0; vertexIndex < vertices.Length; ++vertexIndex)
+				{
+					Vector2Int globalIdx = Utils.GetXYFromIndex(vertexIndex + offset, count.x);
+					Vector2 fCount = count;
+					Vector2 uv = globalIdx / fCount;
+					Vector2 pos = uv * size;
 
+					var height = Utils.SampleColorFromNativeArray(uv, heightData, textureSize);
+					Vector3 pos3 = new Vector3(pos.x, height.r / (255f) * heightScale, pos.y);
+					VertexData vd = new VertexData();
+					vd.pos = pos3;
+					vd.uv = uv;
+					vertices[vertexIndex] = vd;
 
-
-		//	}
-		//}
+					if ((vertexIndex * 6) < indices.Length)
+					{
+						uint p1 = (uint)vertexIndex;
+						uint p2 = ((uint)vertexIndex + 1);
+						uint p3 = ((uint)vertexIndex + (uint)count.x);
+						uint p4 = ((uint)vertexIndex + (uint)count.x + 1);
+						indices[(vertexIndex * 6) + 0] = p1;
+						indices[(vertexIndex * 6) + 1] = p3;
+						indices[(vertexIndex * 6) + 2] = p2;
+						indices[(vertexIndex * 6) + 3] = p2;
+						indices[(vertexIndex * 6) + 4] = p3;
+						indices[(vertexIndex * 6) + 5] = p4;
+					}
+				}
+			}
+		}
 
 		private static VertexData ComputeVertexData(int i, Vector2Int verticeCount, Vector2 size, float heightScale, NativeArray<Color32> heightMapData, Vector2Int pixelCount)
 		{
@@ -79,49 +106,53 @@ namespace _018_TerraGenCPU_MeshThread
 		{
 			Stopwatch sw = Stopwatch.StartNew();
 
-			var verticesCount = count.x * count.y;
-			var triCount = (count.x - 1) * (count.y - 1) * 2 * 3;
-
-			vertices = new NativeArray<VertexData>(verticesCount, Allocator.Persistent);
-			indices = new NativeArray<uint>(triCount, Allocator.Persistent);
-
 			var heightData = heightMap.GetRawTextureData<RGB>();
 			Vector2Int textureSize = new Vector2Int(heightMap.width, heightMap.height);
 			var max = Utils.GetArrayIdxClamp(textureSize - Vector2Int.one, textureSize);
-			if (max != heightData.Length-1)
+			if (max != heightData.Length - 1)
 			{
 				Debug.Log($"Invalid texture size {max} {heightData.Length} {textureSize} {textureSize.x * textureSize.y} format:{heightMap.format}");
 				return;
 			}
 
-			int tIdx = 0;
-			for (int i = 0; i < vertices.Length; ++i)
+			int jobsCount = JobsUtility.JobWorkerMaximumCount;
+
+			var verticesCount = count.x * count.y;
+			var indicesCount = (count.x - 1) * (count.y - 1) * 2 * 3;
+			int verticesCountPartial = verticesCount / jobsCount;			
+			int indicesCountPartial = indicesCount / jobsCount;
+
+			Debug.Log($"jobsCount:{jobsCount} indicesCountPartial:{indicesCountPartial} verticesCountPartial:{verticesCountPartial}");
+
+			MeshJob[] jobArray = new MeshJob[jobsCount];
+			JobHandle[] jobHandleArray = new JobHandle[jobsCount];
+			int offset = 0;
+			for (int j = 0; j < jobsCount; ++j)
 			{
-				Vector2Int idx = Utils.GetXYFromIndex(i, count.x);
-				Vector2 fCount = count;
-				Vector2 uv = idx / fCount;
-				Vector2 pos = uv * size;
-
-				var height = Utils.SampleColorFromNativeArray(uv, heightData, textureSize);
-				Vector3 pos3 = new Vector3(pos.x, height.r / (255f) * heightScale, pos.y);
-				VertexData vd = new VertexData();
-				vd.pos = pos3;
-				vd.uv = uv;
-				vertices[i] = vd;
-
-				if (tIdx < indices.Length)
+				if (j == jobsCount-1)
 				{
-					uint p1 = (uint)i;
-					uint p2 = ((uint)i + 1);
-					uint p3 = ((uint)i + (uint)count.x);
-					uint p4 = ((uint)i + (uint)count.x + 1);
-					indices[tIdx++] = p1;
-					indices[tIdx++] = p3;
-					indices[tIdx++] = p2;
-					indices[tIdx++] = p2;
-					indices[tIdx++] = p3;
-					indices[tIdx++] = p4;
+					verticesCountPartial += verticesCount % jobsCount;
+					indicesCountPartial += indicesCount % jobsCount;
 				}
+				MeshJob job;
+				job.vertices = new NativeArray<VertexData>(verticesCountPartial, Allocator.TempJob);;
+				job.indices = new NativeArray<uint>(indicesCountPartial, Allocator.TempJob);
+				job.heightData = heightData;
+				job.count = count;
+				job.size = size;
+				job.textureSize = textureSize;
+				job.heightScale = heightScale;
+				job.offset = offset;
+				jobHandleArray[j] = job.Schedule();
+
+				jobArray[j] = job;
+
+				offset += verticesCountPartial;
+			}
+
+			for (int j = 0; j < jobsCount; ++j)
+			{
+				jobHandleArray[j].Complete();
 			}
 
 			MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
@@ -130,29 +161,37 @@ namespace _018_TerraGenCPU_MeshThread
 			mesh.indexFormat = IndexFormat.UInt32;
 
 			// specify vertex count and layout
+			var updateFlags = MeshUpdateFlags.DontNotifyMeshUsers | MeshUpdateFlags.DontRecalculateBounds | MeshUpdateFlags.DontResetBoneBounds | MeshUpdateFlags.DontValidateIndices;
 			mesh.SetVertexBufferParams(verticesCount, vertexLayout);
-			mesh.SetVertexBufferData(vertices, 0, 0, verticesCount);
-			mesh.SetIndexBufferParams(triCount, IndexFormat.UInt32);
-			mesh.SetIndexBufferData(indices, 0, 0, triCount);
+			mesh.SetIndexBufferParams(indicesCount, IndexFormat.UInt32);
+			int meshVerticesStart = 0;
+			int meshIndicesStart = 0;
+			for (int j = 0; j < jobsCount; ++j)
+			{
+				var job = jobArray[j];
+
+				mesh.SetVertexBufferData(job.vertices, 0, meshVerticesStart, job.vertices.Length, 0, updateFlags);
+				mesh.SetIndexBufferData(job.indices, 0, meshIndicesStart, job.indices.Length, updateFlags);
+
+				meshVerticesStart += job.vertices.Length;
+				meshIndicesStart += job.indices.Length;
+			}
 			mesh.subMeshCount = 1;
-			mesh.SetSubMesh(0, new SubMeshDescriptor(0, triCount, MeshTopology.Triangles));
-			mesh.RecalculateBounds();
+			mesh.SetSubMesh(0, new SubMeshDescriptor(0, indicesCount, MeshTopology.Triangles));
+			mesh.RecalculateBounds(); 
 
 			meshFilter.mesh = mesh;
 
-			Debug.Log($"creation time:{sw.ElapsedMilliseconds}ms");
-		}
+			for (int j = 0; j < jobsCount; ++j)
+			{
+				var job = jobArray[j];
+				if (job.indices.IsCreated)
+					job.indices.Dispose();
+				if (job.vertices.IsCreated)
+					job.vertices.Dispose();
 
-		private void OnDestroy()
-		{
-			if (vertices.IsCreated)
-			{
-				vertices.Dispose();
 			}
-			if (indices.IsCreated)
-			{
-				indices.Dispose();
-			}
+			Debug.Log($"creation time:{sw.ElapsedMilliseconds}ms");
 		}
 	}
 
