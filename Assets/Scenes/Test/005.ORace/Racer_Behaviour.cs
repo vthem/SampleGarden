80486a5bf268c418ede183976cf61a7d33f49877
Vincent Thémereau <v@themereau.com>
2022-10-09 21:06:39 +0200

    ORace 3/n

--- Assets/Scenes/Test/005.ORace/Racer_Behaviour.cs
+++ Assets/Scenes/Test/005.ORace/Racer_Behaviour.cs
@@ -1,24 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Collections;
 
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
+
+[System.Serializable]
+public class AltitudeModule
+{
+	public bool enable = false;
+	public float downAltitudeSmooth = 1f;
+	public float verticalMaxSpeed = 1f;
+	public float hoverHeight = 0.5f;
+
+	private Vector3 verticalVelocity;
+
+	public void Update(Transform transform, Vector3 groundPoint)
+	{
+		if (!enable)
+			return;
+
+		var groundDir = (groundPoint - transform.position).normalized;
+		var hoverTarget = groundPoint - groundDir * hoverHeight;
+		transform.localPosition = Vector3.SmoothDamp(transform.localPosition, hoverTarget, ref verticalVelocity, downAltitudeSmooth, verticalMaxSpeed);
+	}
+}
+
+[System.Serializable]
+public class RollModule
+{
+	[Range(0, 1f)] public float rollSmooth = 1f;
+	[Range(10, 90f)] public float maxAngle = 45f;
+
+	public Quaternion rotation;
+	private float currentVelocity;
+	public float targetZAngle;
+	public float currentZAngle;
+	[Range(-1, 1)] public float input = 0f;
+
+	public void Update(Transform transform)
+	{
+		var hInputAxis = input;
+		if (hInputAxis == 0)
+		{
+			input = hInputAxis = Input.GetAxis("Horizontal");
+		}
+		
+		targetZAngle = -hInputAxis * maxAngle;
+		currentZAngle = transform.localEulerAngles.z;
+		var newZAngle = Mathf.SmoothDampAngle(currentZAngle, targetZAngle, ref currentVelocity, rollSmooth);
+		//rotation = Quaternion.Euler(0, 0, Mathf.DeltaAngle(currentZAngle, newZAngle));
+		rotation = Quaternion.AngleAxis(Mathf.DeltaAngle(currentZAngle, newZAngle), transform.forward);
+	}
+}
+
+[System.Serializable]
+public class YawModule
+{
+	[Range(0, 90)] public float angularSpeed = 45f;
+	public float input;
+	public Quaternion rotation;
+
+	public void Update(Transform transform)
+	{
+		//rotation = Quaternion.Euler(0, angularSpeed * Time.deltaTime * input, 0);
+		rotation = Quaternion.AngleAxis(-angularSpeed * Time.deltaTime * input, transform.up);
+	}
+}
+
 public class Racer_Behaviour : MonoBehaviour
 {
-	public float ySmooth = 1f;
-	public float yMaxSpeed = 1f;
+	public AltitudeModule altitudeModule;
+	public RollModule rollModule;
+	public YawModule yawModule;
+
 	public float zMaxSpeed = 1f;
-	public float hoverHeight = 0.5f;
+	
 	public float forwardSmoothTime = 1f;
 	public float forwardMaxSpeed = 1f;
 
 	public float yAngularSpeed = 1f;
 
-	private bool enableFakeGravity = false;
-	private Vector3 yVelocity;
+	
 	private Vector3 velocity;
 	private Vector3 forwardVelocity;
 	private float heading = 0f; // [0, 360[ degree
@@ -99,40 +165,39 @@ public class Racer_Behaviour : MonoBehaviour
 		UpdateGroundPositionInfoArray();
 
 		var gpCenter = groundPositionInfoArray[GroundPositionInfo.CenterIndex];
-		if (enableFakeGravity && gpCenter.Found)
+		if (gpCenter.Found)
 		{
-			var groundDir = (gpCenter.Position - transform.position).normalized;
-			var hoverTarget = gpCenter.Position - groundDir * hoverHeight;
-			transform.localPosition = Vector3.SmoothDamp(transform.localPosition, hoverTarget, ref yVelocity, ySmooth, yMaxSpeed);
+			altitudeModule.Update(transform, gpCenter.Position);
 		}
 
-		var hInputAxis = Input.GetAxis("Horizontal");
-		heading += yAngularSpeed * Time.deltaTime * hInputAxis;
-		if (heading > 360)
-		{
-			heading = heading - 360f;
-		}
-		else if (heading < 0)
-		{
-			heading += 360f;
-		}
+		//var hInputAxis = Input.GetAxis("Horizontal");
+		//heading += yAngularSpeed * Time.deltaTime * hInputAxis;
+		//if (heading > 360)
+		//{
+		//	heading = heading - 360f;
+		//}
+		//else if (heading < 0)
+		//{
+		//	heading += 360f;
+		//}
 
-		if (TryComputeForward(out Vector3 forward) && TryComputeRight(out Vector3 right))
-		{
-			Vector3 up = Vector3.Cross(forward, right);
+		//if (TryComputeForward(out Vector3 forward) && TryComputeRight(out Vector3 right))
+		//{
+		//	Vector3 up = Vector3.Cross(forward, right);
 
-			// rotate forward according
-			var rot = Quaternion.AngleAxis(heading, up);
-			forward = Vector3.SmoothDamp(forward, rot * forward, ref forwardVelocity, forwardSmoothTime, forwardMaxSpeed);
-			Debug.DrawLine(transform.position, transform.position + transform.forward * 2, Color.blue);
-			Debug.DrawLine(transform.position, transform.position + forward * 2, Color.green);
-			transform.rotation = Quaternion.LookRotation(forward, up);
-		}
-	}
+		//	// rotate forward according
+		//	var rot = Quaternion.AngleAxis(heading, up);
+		//	forward = Vector3.SmoothDamp(forward, rot * forward, ref forwardVelocity, forwardSmoothTime, forwardMaxSpeed);
+		//	Debug.DrawLine(transform.position, transform.position + transform.forward * 2, Color.blue);
+		//	Debug.DrawLine(transform.position, transform.position + forward * 2, Color.green);
+		//	//transform.rotation = Quaternion.LookRotation(forward, up);
+		//}
 
-	private void OnDrawGizmos()
-	{
+		rollModule.Update(transform);
+		yawModule.input = rollModule.input;
+		yawModule.Update(transform);
 
+		transform.rotation *= rollModule.rotation/* * yawModule.rotation*/;
 	}
 
 #if UNITY_EDITOR
@@ -144,7 +209,7 @@ public class Racer_Behaviour : MonoBehaviour
 			DrawDefaultInspector();
 
 			var racer = target as Racer_Behaviour;
-			racer.enableFakeGravity = EditorGUILayout.Toggle("Enable Fake Gravity", racer.enableFakeGravity);
+			racer.altitudeModule.enable = EditorGUILayout.Toggle("Enable Fake Gravity", racer.altitudeModule.enable);
 		}
 	}
 #endif
